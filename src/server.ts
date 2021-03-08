edb2583d21506bdb1cf9baf820260ea2dd2698a0
Lucas Sousa <frempyrean@gmail.com>
2021-03-08 19:40:36 -0300

    feat: add route placeholders

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 
 const app = express()
@@ -6,5 +6,33 @@ const app = express()
 /** Adding CORS and JSON */
 app.use(cors())
 app.use(express.json())
+
+function checksExistsUserAccount(req: Request, res: Response, next: NextFunction): void {
+  // Complete aqui
+}
+
+app.post('/users', (req: Request, res: Response): void => {
+  // Complete aqui
+})
+
+app.get('/todos', checksExistsUserAccount, (req: Request, res: Response): void => {
+  // Complete aqui
+})
+
+app.post('/todos', checksExistsUserAccount, (req: Request, res: Response): void => {
+  // Complete aqui
+})
+
+app.put('/todos/:id', checksExistsUserAccount, (req: Request, res: Response): void => {
+  // Complete aqui
+})
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (req: Request, res: Response): void => {
+  // Complete aqui
+})
+
+app.delete('/todos/:id', checksExistsUserAccount, (req: Request, res: Response): void => {
+  // Complete aqui
+})
 
 export default app
