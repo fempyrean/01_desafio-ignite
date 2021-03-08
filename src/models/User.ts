4ef0c56bea5233fa25de4cf3c6f6bce15e8d783c
Lucas Sousa <frempyrean@gmail.com>
2021-03-08 19:58:21 -0300

    feat: create user model

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity('user')
+class User {
+  @PrimaryColumn()
+  readonly id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  username: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @CreateDateColumn()
+  deleted_at: Date
+
+  @CreateDateColumn()
+  updated_at: Date
+
+  constructor () {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
+
+export { User }
