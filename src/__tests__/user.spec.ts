8e5799dcd8d0b86d66f21fcdd31ced704a877817
Lucas Sousa <frempyrean@gmail.com>
2021-03-09 11:24:10 -0300

    test: should be able to create a new user

---
+++ src/__tests__/user.spec.ts
@@ -0,0 +1,24 @@
+import { getConnection } from 'typeorm'
+import app from '../server'
+import createConnection from '../database'
+import request from 'supertest'
+
+describe('Users', () => {
+  beforeAll(async () => {
+    const connection = await createConnection()
+    await connection.runMigrations()
+  })
+  afterAll(async () => {
+    const connection = getConnection()
+    await connection.dropDatabase()
+    await connection.close()
+  })
+
+  test('Should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'any_name',
+      username: 'any_username'
+    })
+    expect(response.status).toBe(201)
+  })
+})
